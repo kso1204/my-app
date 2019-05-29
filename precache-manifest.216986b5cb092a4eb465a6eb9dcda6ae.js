7a59c878a644ecbe0d09c7903d96a9349fe2f5df
kso1204 <kso1204@geni-pco.com>
2019-05-29 10:33:39 +0900

    Updates

---
+++ precache-manifest.216986b5cb092a4eb465a6eb9dcda6ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.1cff5780.js"
+  },
+  {
+    "revision": "76d71706eb534dd423d1",
+    "url": "/my-app/static/js/main.c0efa610.chunk.js"
+  },
+  {
+    "revision": "3537fedd551c697bfa86",
+    "url": "/my-app/static/js/2.3f067ba8.chunk.js"
+  },
+  {
+    "revision": "76d71706eb534dd423d1",
+    "url": "/my-app/static/css/main.91154059.chunk.css"
+  },
+  {
+    "revision": "026deff7fba63c023fe67a1702d11e30",
+    "url": "/my-app/index.html"
+  }
+];
